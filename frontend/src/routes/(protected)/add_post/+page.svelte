5dde1b860d647be6dacfa3ba241eac642b0009dd
Mateusz Goik <mateuszgoik1@wp.pl>
2023-09-30 21:17:41 +0200

    Add edit_post and add_post

---
+++ frontend/src/routes/(protected)/add_post/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import PostEditor from '$lib/components/PostEditor.svelte';
+
+	let name = '';
+	let description = '';
+	let files: FileList | undefined;
+	let quill: any;
+
+	function submit(e: SubmitEvent) {
+		e.preventDefault();
+		console.log({
+			name,
+			description,
+			content: quill.root.innerHTML
+		});
+	}
+</script>
+
+<PostEditor
+	title="Edit course"
+	submitText="Edit"
+	bind:name
+	bind:description
+	bind:files
+	bind:quill
+	on:submit={submit}
+/>
