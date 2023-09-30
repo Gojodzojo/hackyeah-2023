7eda71452e1840ea38982f497f80333ace179e66
Mateusz Goik <mateuszgoik1@wp.pl>
2023-09-30 23:47:39 +0200

    Fixes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,22 @@
 import express from "express";
-
+import cors from 'cors';
 import auth from "./endpoints/auth";
 import user from "./endpoints/user";
 import course from "./endpoints/course";
 import asset from "./endpoints/asset";
-
-const PORT = 3000;
+import { API_PORT } from "./constants";
 
 const app = express();
 
 app.use(express.json())
+app.use(cors());
 
 app.use("/auth", auth);
 app.use("/user", user);
 app.use("/course", course);
 app.use("/asset", asset);
 
-app.listen(PORT, () => {
-    console.log(`Server started at port ${PORT}`);
+app.listen(API_PORT, () => {
+    console.log(`Server started at port ${API_PORT}`);
 });
 
--- frontend/src/lib/scripts/apiFetch.ts
+++ frontend/src/lib/scripts/apiFetch.ts
@@ -1,15 +1,13 @@
 import { get } from 'svelte/store';
 import { authStore } from './authentication';
 import { API_PORT } from 'backend/src/constants';
-import { BAD_ACCESS_TOKEN, type StatusResponse } from 'backend/src/helperTypes';
-import type { BAD_REFRESH_TOKEN, TokenLoginRequest, TokenLoginResponse, USR_NOT_EXISTS } from 'backend/src/endpoints/token';
 
 const API_BASE = import.meta.env.DEV ? `http://localhost:${API_PORT}` : '';
 
 export const NO_CONNECTION_TO_SERVER = 'No connection to server';
 export const UNAUTHORISED_USER = 'Unauthorised user';
 
-export async function apiFetch<Resp extends object, Req extends object = object>(url: string, method: string, body?: Req): Promise<Resp | StatusResponse<typeof NO_CONNECTION_TO_SERVER>> {
+export async function apiFetch<Resp extends object, Req extends object = object>(url: string, method: string, body?: Req): Promise<Resp | { status: string }> {
 	try {
 		const resp = await fetch(API_BASE + url, {
 			method,
@@ -27,27 +25,12 @@ export async function apiFetch<Resp extends object, Req extends object = object>
 export async function protectedApiFetch
 	<Resp extends object, Req extends object = object>
 	(url: string, method: string, body?: Req):
-	Promise<Resp | StatusResponse<typeof NO_CONNECTION_TO_SERVER | typeof UNAUTHORISED_USER | typeof USR_NOT_EXISTS | typeof BAD_REFRESH_TOKEN>> {
+	Promise<Resp | { status: string }> {
 	const auth = get(authStore);
 
 	if (typeof auth === 'string') return { status: UNAUTHORISED_USER };
-	const { accessToken, refreshToken } = auth;
+	const { accessToken } = auth;
 
 	const b = body ? body : {};
-	const resp = await apiFetch<Resp | StatusResponse<typeof BAD_ACCESS_TOKEN>>(url, method, { accessToken, ...b });
-
-	if (!('status' in resp) || resp.status !== BAD_ACCESS_TOKEN) {
-		return resp;
-	};
-
-	const refResp = await apiFetch<TokenLoginResponse, TokenLoginRequest>('/api/token', 'POST', { refreshToken });
-
-	if ('status' in refResp) {
-		authStore.set("LOGGED_OUT");
-		return refResp;
-	}
-
-	authStore.set({ ...refResp, refreshToken });
-
 	return await apiFetch(url, method, { accessToken, ...b });
 }
