a0a5e8c2a060ad7734f769bb73ce1975e45f9342
Patryk Kwasniok <kwasniokpatryk@gmail.com>
2023-09-30 22:39:58 +0200

    Added iamges server

---
+++ backend/src/endpoints/asset.ts
@@ -0,0 +1,49 @@
+import express from "express";
+import multer from "multer";
+import database from "../utils/database";
+
+const router = express.Router();
+const storage = multer.memoryStorage();
+
+const upload = multer({ storage });
+
+router.post("/", upload.single("image"), async (req, res) => {
+    if (req.file == undefined) {
+        res.status(400).end();
+    }
+
+    console.log(req.file);
+
+    const asset = await database.asset.create({
+        data: {
+            mimetype: req.file!.mimetype,
+            data: req.file!.buffer,
+        },
+    });
+
+    res.send({
+        status: 200,
+        data: {
+            url: `/asset/${asset.id}`,
+        },
+    });
+});
+
+router.get("/:id", async (req, res) => {
+    const image = await database.asset.findFirst({
+        where: {
+            id: Number(req.params.id),
+        },
+    });
+
+    if (image == null) {
+        res.status(404).end();
+        return;
+    }
+
+    res.contentType("image/jpeg")
+    res.send(image.data);
+})
+
+export default router;
+
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import auth from "./endpoints/auth";
 import user from "./endpoints/user";
 import course from "./endpoints/course";
+import asset from "./endpoints/asset";
 
 const PORT = 3000;
 
@@ -13,6 +14,7 @@ app.use(express.json())
 app.use("/auth", auth);
 app.use("/user", user);
 app.use("/course", course);
+app.use("/asset", asset);
 
 app.listen(PORT, () => {
     console.log(`Server started at port ${PORT}`);
