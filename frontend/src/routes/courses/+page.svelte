66a89d90359084aff84e6ab6087064de16c90c6d
Mateusz Goik <mateuszgoik1@wp.pl>
2023-09-30 15:42:51 +0200

    Edit courses

--- frontend/src/routes/courses/+page.svelte
+++ frontend/src/routes/courses/+page.svelte
@@ -1,1 +1,17 @@
-courses
+<script lang="ts">
+	const courses = [
+		{
+			id: 1,
+			creatorId: 1,
+			name: 'Kurs 1',
+			description: 'cos'
+		}
+	];
+</script>
+
+<div class="p-20">
+	<h1 class="h1">Courses</h1>
+	{#each courses as course}
+		<!-- dodać coś takiego https://www.skeleton.dev/elements/cards -->
+	{/each}
+</div>
